a55454e3edfb763b47fafe521d75edc46cbea592
yzhang7 <yi_zhang2@intuit.com>
2022-07-30 11:04:54 -0700

    update

--- src/main/java/com/yi/springsecuritydemo/config/WebSecurityConfig.java
+++ src/main/java/com/yi/springsecuritydemo/config/WebSecurityConfig.java
@@ -1,30 +1,10 @@
 package com.yi.springsecuritydemo.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+import org.springframework.stereotype.Component;
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.withDefaultPasswordEncoder().username("yzhang").password("pass").roles("USER").build();
-        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
-        inMemoryUserDetailsManager.createUser(userDetails);
-        return inMemoryUserDetailsManager;
-    }
+@Component
+public class WebSecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
-        http.authorizeRequests().anyRequest().authenticated();
-    }
+
 }

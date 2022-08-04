82caf2f33459e3b177771fcb324fded04acc2b7f
yzhang7 <yi_zhang2@intuit.com>
2022-08-03 21:25:09 -0700

    fix

--- src/main/java/com/yi/springsecuritydemo/controller/WebController.java
+++ src/main/java/com/yi/springsecuritydemo/controller/WebController.java
@@ -4,18 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
-import java.time.ZonedDateTime;
 
 @RestController
 public class WebController {
 
     @GetMapping(value = "/time", produces = "application/json")
-    public ZonedDateTime currentTime(Principal principal) {
-        return ZonedDateTime.now();
+    public String currentTime(Principal principal) {
+        return "ZonedDateTime.now()";
     }
 
     @GetMapping(value = "/secret", produces = "application/json")
-    public ZonedDateTime currentSecretTime() {
-        return ZonedDateTime.now();
+    public String currentSecretTime() {
+        return "ZonedDateTime.now()";
     }
 }
